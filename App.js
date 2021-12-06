7c823e520744a337f10baff250137b197e61f018
smfriedm <94500978+smfriedm@users.noreply.github.com>
2021-12-06 13:02:38 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { NavigationContainer } from "react-navigation";
+import { NavigationContainer, NavigationEvents } from "react-navigation";
 import React from "react";
-import { createAppContainer } from "react-navigation";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 import { createStackNavigator } from "react-navigation-stack";
 import {StyleSheet,View} from 'react-native';
@@ -10,12 +10,14 @@ import RecipeListScreen from "./src/screens/RecipeListScreen";
 import ShoppingListScreen from "./src/screens/ShoppingListScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
 import SignInScreen from "./src/screens/SignInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import addItemScreen from "./src/screens/addItemScreen";
 import Icon from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather'
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import FoodDetailScreen from "./src/screens/FoodDetailScreen";
-import FoodAddScreen from "./src/screens/FoodAddScreen";
+import {Provider as AuthProvider} from './src/contexts/AuthContext'
 import { Provider as HeroProvider } from "./src/contexts/FoodContext";
+import FoodDetailScreen from "./src/screens/FoodDetailScreen";
 
 
 const styles = StyleSheet.create({
@@ -26,69 +28,122 @@ const styles = StyleSheet.create({
   }
 })
 
-const TabNavigator = createMaterialBottomTabNavigator(
-  {
-	Detail:{
-    screen: FoodDetailScreen,
-	showIcon: 'false'
-	},
-	Add:{
-    screen: FoodAddScreen,
-	showIcon: 'false'
-	},
-  Pantry:{
-    screen: PantryScreen,
-    navigationOptions:{
-      tabBarIcon: ({tintColor}) => (
-        <View>
-          <Icon name={'home'} size={25}/>
-        </View>
-      )
-    }
-  },
-  Shopping:{
-    screen: ShoppingListScreen,
-    navigationOptions:{
-      tabBarIcon: ({tintColor}) => (
-        <View>
-          <Feather name={'shopping-cart'} size={25}/>
-        </View>
-      )
-    }
-  },
-  Recipe:{
-    screen: RecipeListScreen,
-    navigationOptions:{
-      tabBarIcon: ({tintColor}) => (
-        <View>
-          <MaterialCommunityIcon name={'chef-hat'} size={25}/>
-        </View>
-      )
-    }
-  },
-  Profile:{
-    screen: ProfileScreen,
-    navigationOptions:{
-      tabBarIcon: ({tintColor}) => (
-        <View>
-          <Icon name={'person-circle'} size={25}/>
-        </View>
-      )
-    }
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignUpScreen,
+    Signin: SignInScreen
   }
-},
-{
-  initialRouteName: 'Pantry',
-  activeColor: '#ffffff',
-  inactiveColor: '#bda1f7',
-  barStyle: { backgroundColor: '#6948f4' },
-}
-)
+  ),
+  mainFlow: createMaterialBottomTabNavigator({
+        pantryFlow: createStackNavigator({
+        Pantry: {
+          screen: PantryScreen,
+          navigationOptions:{
+            tabBarLabel: 'Pantry',
+            tabBarIcon: ({tintColor}) => (
+              <View>
+                <Icon name={'home'} size={25}/>
+              </View>
+            )
+          }
+        },
+        AddItem: addItemScreen,
+		Detail: FoodDetailScreen
+      }), //end stack navigator
+        ShoppingList: {
+              screen: ShoppingListScreen,
+              navigationOptions:{
+                tabBarIcon: ({tintColor}) => (
+                  <View>
+                    <Feather name={'shopping-cart'} size={25}/>
+                  </View>
+                )
+              }
+            },
+        RecipeList: {
+              screen: RecipeListScreen,
+              navigationOptions:{
+                tabBarIcon: ({tintColor}) => (
+                  <View>
+                    <MaterialCommunityIcon name={'chef-hat'} size={25}/>
+                  </View>
+                )
+              }
+            },
+        Account: {
+              screen: ProfileScreen,
+              navigationOptions:{
+                tabBarIcon: ({tintColor}) => (
+                  <View>
+                    <Icon name={'person-circle'} size={25}/>
+                  </View>
+                )
+              }
+            }
+          },
+  ) //end mainFlow
+}) //end switchNavigator
 
-const App = createAppContainer(TabNavigator);
+// const TabNavigator = createMaterialBottomTabNavigator(
+//   {
+  // Pantry:{
+  //   screen: PantryScreen,
+  //   navigationOptions:{
+  //     tabBarIcon: ({tintColor}) => (
+  //       <View>
+  //         <Icon name={'home'} size={25}/>
+  //       </View>
+  //     )
+  //   }
+  // },
+//   Shopping:{
+//     screen: ShoppingListScreen,
+//     navigationOptions:{
+//       tabBarIcon: ({tintColor}) => (
+//         <View>
+//           <Feather name={'shopping-cart'} size={25}/>
+//         </View>
+//       )
+//     }
+//   },
+//   Recipe:{
+//     screen: RecipeListScreen,
+//     navigationOptions:{
+//       tabBarIcon: ({tintColor}) => (
+//         <View>
+//           <MaterialCommunityIcon name={'chef-hat'} size={25}/>
+//         </View>
+//       )
+//     }
+//   },
+//   Profile:{
+//     screen: ProfileScreen,
+//     navigationOptions:{
+//       tabBarIcon: ({tintColor}) => (
+//         <View>
+//           <Icon name={'person-circle'} size={25}/>
+//         </View>
+//       )
+//     }
+//   }
+// },
+// {
+//   initialRouteName: 'Pantry',
+//   activeColor: '#ffffff',
+//   inactiveColor: '#bda1f7',
+//   barStyle: { backgroundColor: '#6948f4' },
+// }
+// )
+
+
+const App = createAppContainer(switchNavigator);
 
 export default () => {
-	return <HeroProvider>
+  return (
+    <AuthProvider>
+      <HeroProvider>
 	<App />
 	</HeroProvider>
-}
+    </AuthProvider>
+  )
+};
